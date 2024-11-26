ff37ff2d8b3000ab841d64c9ad8d93d20a3fa02a
maiko <3430maiko@gmail.com>
2024-11-27 05:24:19 +0900

    ファイルの追加

--- src/routes/web.php
+++ src/routes/web.php
@@ -17,6 +17,14 @@ use App\Http\Controllers\ContactController;
 Route::get('/', [ContactController::class, 'index']);
 Route::post('/contacts/confirm', [ContactController::class, 'confirm']);
 Route::post('/contacts', [ContactController::class, 'store']);
+Route::post('/thanks', [ContactController::class, 'store']);
 Route::get('/login', function () {
     return view('auth.login'); // ここに適切なビューを設定
 })->name('login');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/admin', [ContactController::class, 'admin']);
+    Route::get('/search', [ContactController::class, 'search']);
+    Route::post('/delete', [ContactController::class, 'destroy']);
+    Route::post('/export', [ContactController::class, 'export']);
+});
